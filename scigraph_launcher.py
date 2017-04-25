043de1af846de5d0d97c49a3c80fd234115ca676
tmsincomb <tsincomb@ucsd.edu>
2017-04-24 22:22:59 -0700

    Update
    Used sgv instead. Faster runtime and check.ipynb verifies that all
    labels are accounted for and that each list is unique.

--- scigraph_launcher.py
+++ scigraph_launcher.py
@@ -2,7 +2,8 @@ import pandas as pd
 import math
 import subprocess as sb
 from multiprocessing import Pool
-
+from pyontutils.scigraph_client import Vocabulary
+sgv = Vocabulary(basePath='http://localhost:9000/scigraph')
 '''
 git clone https://github.com/SciCrunch/SciGraph;
 cd SciGraph \
@@ -32,30 +33,37 @@ nans = [label for label in list(df.iloc[:, label_index]) if type(label) == float
 print(len(labels))
 labels = list(map(str, labels))
 labels = list(enumerate(labels))
-write_n = open('labels_not_in_NIF2.txt', 'w')
-write_y = open('labels_in_NIF2.txt2', 'w')
 
+write_n = open('labels_not_in_NIF.txt', 'w')
+#write_n = open('labels_in_NIF.txt', 'w')
 
 def worker(label):
     count = label[0]
     label = label[1]
+
     if type(label) == str:
         label_bash_ready = ''.join(label.replace('(', '\(').replace(')', '\)'))
     else:
         label_bash_ready = str(label)
-    output = sb.getoutput('python3 scig.py -l s '+label_bash_ready)
-    iris = [value.split()[1] for value in output.split('\n') if 'iri:' in value]
+
+    #output = sb.getoutput('python3 scig.py -l s '+label_bash_ready)
+    #iris = [value.split()[1] for value in output.split('\n') if 'iri:' in value]
+    iris = [query['iri'] for query in sgv.searchByTerm(label)]
+    if iris == []: iris = [query['iri'] for query in sgv.findByTerm(label)]
+
     if count % 5 == 0: print(count)
+
     if 'NIF' not in ''.join(iris) and iris != []:
         return label
+
     else:
         return '$'
 
 p = Pool()
 no_list = p.map(worker, labels)
+print('length of list:', len(no_list))
 for value in no_list:
     if value == '$': continue
     write_n.write(value+ '\n')
 
 write_n.close()
-write_y.close()
